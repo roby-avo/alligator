a4261c06598767b8f0685813ffec1d16f7ed9b68
Roberto <r.avogadro@campus.unimib.it>
2023-09-06 00:25:39 +0200

    add round

--- work/process/phases/storage.py
+++ work/process/phases/storage.py
@@ -43,9 +43,9 @@ class Storage:
                 wc = []
                 rank = candidates[0:20] if len(candidates) > 0 else []
                 if len(candidates) > 0:
-                    candidates[0]["delta"] = candidates[0]["rho"] - candidates[1]["rho"]
+                    candidates[0]["delta"] = round(candidates[0]["rho"] - candidates[1]["rho"], 3)
                 for candidate in candidates[1:]:
-                    candidate["delta"] = candidates[0]["rho"] - candidate["rho"]
+                    candidate["delta"] = round(candidates[0]["rho"] - candidate["rho"], 3)
                     candidate["score"] = round((1-K) * candidate["rho"] + K * candidate["delta"], 3)
                 
                     if (candidates[0]["score"] - candidate["score"]) < THRESHOLD:
