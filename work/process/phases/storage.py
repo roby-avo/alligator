fc3c72458d14c71de302c55bc46a04866e976f37
Roberto <r.avogadro@campus.unimib.it>
2023-09-06 00:15:57 +0200

    test new delta

--- work/process/phases/storage.py
+++ work/process/phases/storage.py
@@ -42,10 +42,9 @@ class Storage:
                 candidates = cell.candidates()
                 wc = []
                 rank = candidates[0:20] if len(candidates) > 0 else []
-                if len(candidates) > 1:
-                    delta = round(candidates[0]["rho"] - candidates[1]["rho"], 3)
+                
                 for candidate in candidates:
-                    candidate["delta"] = delta  
+                    candidate["delta"] = round(1 - (candidates[0]["rho"] - candidates["rho"]), 3)  
                     candidate["score"] = round((1-K) * candidate["rho"] + K * candidate["delta"], 3)
                 
                     if (candidates[0]["score"] - candidate["score"]) < THRESHOLD:
