ac0b08ff736a20eb337fa21ef49e9d027b2ee7b3
roby-avo <r.avogadro@campus.unimib.it>
2023-09-26 23:36:57 +0200

    integrated description in lookup search

--- work/process/phases/lookup.py
+++ work/process/phases/lookup.py
@@ -25,7 +25,7 @@ class Lookup:
         for i, cell in enumerate(cells):
             if i in self._target["NE"]:
                 types = self._types.get(str(i))
-                candidates = self._get_candidates(cell, id_row, types)
+                candidates = self._get_candidates(cell, row_text, id_row, types)
                 is_subject = i == self._target["SUBJ"]
                 row.add_ne_cell(cell, row_text, candidates, i, is_subject)
             elif i in self._target["LIT"]:
@@ -35,15 +35,15 @@ class Lookup:
         return row
 
     
-    def _get_candidates(self, cell, id_row, types):
+    def _get_candidates(self, cell, row_text, id_row, types):
         candidates = []
         result = None
         try:
             if len(str(cell)) > 0 and str(cell).lower() != "nan":
-                result = self._lamAPI.lookup(cell, limit=100)
+                result = self._lamAPI.lookup(cell, description=row_text, limit=100)
                 if cell not in result:
                     raise Exception("Error from lamAPI")
-            candidates = result[cell]    
+                candidates = result[cell]    
         except Exception as e:
             self._log_c.insert_one({
                 'datasetName': self._dataset_name,
--- work/process/wrapper/lamAPI.py
+++ work/process/wrapper/lamAPI.py
@@ -139,7 +139,7 @@ class LamAPI():
         return result
 
 
-    def lookup(self, string, ngrams=False, fuzzy=False, types=None, limit=100):
+    def lookup(self, string, ngrams=False, fuzzy=False, types=None, description=None, limit=100):
         params = {
             'token': LAMAPI_TOKEN,
             'name': string,
@@ -147,6 +147,7 @@ class LamAPI():
             'fuzzy': fuzzy,
             'types': types,
             'kg': self.kg,
+            'description': description,
             'limit': limit
         }
         result = self.__submit_get(self._url.lookup_url(), params)
