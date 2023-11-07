f6db4bb509cf9f82e65f210d99742163dbfd01b9
roby-avo <r.avogadro@campus.unimib.it>
2023-11-07 11:27:18 +0100

    Add urljoin to URLs.py

--- work/process/wrapper/URLs.py
+++ work/process/wrapper/URLs.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin
+
 class URLs:
     def __init__(self, base_url, response_format="json"):
         self.format = response_format
@@ -25,26 +27,26 @@ class URLs:
         return self.base_url
 
     def lookup_url(self):
-        return self.base_url + self.lookup
-
+        return urljoin(self.base_url, self.lookup)
+        
     def entities_labels_url(self):
-        return self.base_url + self.entities_labels
+        return urljoin(self.base_url, self.entities_labels)
 
     def entities_objects_url(self):
-        return self.base_url + self.entities_objects
+        return urljoin(self.base_url, self.entities_objects)
 
     def entities_predicates_url(self):
-        return self.base_url + self.entities_predicates    
+        return urljoin(self.base_url, self.entities_predicates)
 
     def entities_types_url(self):
-        return self.base_url + self.entities_types
-
+        return urljoin(self.base_url, self.entities_types)
+    
     def entities_literals_url(self):
-        return self.base_url + self.entities_literals
+        return urljoin(self.base_url, self.entities_literals)
 
     def literal_recognizer_url(self):
-        return self.base_url + self._literal_recognizer
+        return urljoin(self.base_url, self._literal_recognizer)
 
     def column_analysis_url(self):
-        return self.base_url + self._column_analysis
+        return urljoin(self.base_url, self._column_analysis)
         
